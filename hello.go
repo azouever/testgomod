f1a644a62b9ec7fe2e4b1688261ee45821a691c8
kaixuan.xu <kaixuan.xu@haoji-hr.com>
2020-11-08 11:58:40 +0800

    test printf

--- hello.go
+++ hello.go
@@ -1,6 +1,18 @@
-package testgomod
+package main
 
-func Hello(name string) string {
+import "fmt"
+
+var name = "xkx"
+
+func hello(name string) string {
 	return "hello, " + name
 }
 
+func echo(str string) string {
+	return str
+}
+
+func main() {
+	age := 25
+	fmt.Printf("hello, %s ,%d .from vscode",name,age)
+}
